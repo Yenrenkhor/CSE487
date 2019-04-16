0209077891a79cc902c8511c2bc8530e720522a6
Yenrenkhor <khoryenr@buffalo.edu>
2019-04-16 13:43:05 -0400

    Updated with subtopics
    500 articles

--- NYTimes/nyt_article.py
+++ NYTimes/nyt_article.py
@@ -6,6 +6,13 @@ with open("nyt_data.txt", "rt") as f:
     urls = [url.strip() for url in f.readlines()]
     print(urls)
 
+# list = list(set(urls))
+# dup = []
+# for item in list:
+#     if item not in dup:
+#         dup.append(item)
+# print(dup)
+
 for url in range(len(urls)):
     r = requests.get(urls[url])
     soup = bs(r.text, 'html.parser')
--- NYTimes/sample_nyt.py
+++ NYTimes/sample_nyt.py
@@ -2,13 +2,21 @@ from nytimesarticle import articleAPI
 import json
 import requests
 
-r = requests.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Stock+Market&begin_date=20190301&end_date=20190323&api-key=WC1NjsQEi1IGxG4PY3hT8AqRCq6xCogy")
+# r = requests.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Stock+Market&begin_date=20190411&end_date=20190420&api-key=WC1NjsQEi1IGxG4PY3hT8AqRCq6xCogy")
+# r = requests.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Tech+Stock&begin_date=20190411&end_date=20190420&api-key=WC1NjsQEi1IGxG4PY3hT8AqRCq6xCogy")
+# r = requests.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Retail+Stock&begin_date=20190411&end_date=20190420&api-key=WC1NjsQEi1IGxG4PY3hT8AqRCq6xCogy")
+# r = requests.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Economy+Stock&begin_date=20190411&end_date=20190420&api-key=WC1NjsQEi1IGxG4PY3hT8AqRCq6xCogy")
+r = requests.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Financial+Stock&begin_date=20190411&end_date=20190420&api-key=WC1NjsQEi1IGxG4PY3hT8AqRCq6xCogy")
+
+
 data = r.json()
 # api = articleAPI("WC1NjsQEi1IGxG4PY3hT8AqRCq6xCogy")
 # articles = api.search(q="Wall+Street", begin_date=20191001)
+print(data)
 url = data["response"]["docs"]
+for i in url:
+    print(i["web_url"])
 print(len(url))
-
 
 with open('nyt_data.txt', 'a') as f:
 
@@ -16,5 +24,5 @@ with open('nyt_data.txt', 'a') as f:
 
         f.write("%s\n" % str(row["web_url"]))
 
-json_file = json.dumps(url, indent=1)
-print(json_file)
+# json_file = json.dumps(url, indent=1)
+# print(json_file)
